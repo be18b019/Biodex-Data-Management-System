5c1aba9828880fac27f86dc2155c7565f832699f
RishadArnabHowlader <rishadarnab@outlook.com>
2020-12-20 11:20:38 +0100

    DB Corrections and HL7 Dummy
    haben paar Datenbanken Korrekturen vorgenommen und das mit den Dummy Daten anzeigen erledigt

--- Database/currently used postgreSQL_material/Version 2/BiodexpostgreSQLV2Script.sql
+++ Database/currently used postgreSQL_material/Version 2/BiodexpostgreSQLV2Script.sql
@@ -14,35 +14,35 @@ DROP DATABASE IF EXISTS BiodexV2;
 CREATE DATABASE BiodexV2;
 \c biodexv2;
 
+DROP TABLE IF EXISTS uses;
+DROP TABLE IF EXISTS givesInforamtion;
+DROP TABLE IF EXISTS mayCONTAIN;
 DROP TABLE IF EXISTS medicalDATA;
 DROP TABLE IF EXISTS personalDATA;
 DROP TABLE IF EXISTS elgaReport;
-DROP TABLE IF EXISTS biodex_report;
+DROP TABLE IF EXISTS exercise_data;
 DROP TABLE IF EXISTS settings;
+DROP TABLE IF EXISTS biodex_report;
 DROP TABLE IF EXISTS report_result;
-DROP TABLE IF EXISTS proband;
 
 
 
 
-
-CREATE TABLE proband( 
-    matriculation_number VARCHAR(50) NOT NULL PRIMARY KEY,
-    proband_name VARCHAR(50));
-
-INSERT INTO proband(matriculation_number, proband_name) 
-VALUES ('be18b500', 'COOLER NAME');
-SELECT * FROM proband;
 
 
 CREATE TABLE report_result(
     report_ID BIGSERIAL NOT NULL PRIMARY KEY,
-    matriculation_number VARCHAR(50) REFERENCES proband(matriculation_number),
     date_time TIMESTAMP);
     
-INSERT INTO report_result(matriculation_number, date_time) 
-VALUES ('be18b500', '2016-06-22 19:10:25-07');
 SELECT * FROM report_result;
+
+
+
+CREATE TABLE biodex_report(
+    biodexreport_ID BIGSERIAL NOT NULL PRIMARY KEY);
+
+SELECT * FROM biodex_report;
+
 
 
 CREATE TABLE settings(
@@ -62,10 +62,12 @@ CREATE TABLE settings(
     Ankle_FLEXION VARCHAR(50),
     Shoulder_ABDUCTION VARCHAR(50),
     Shoulder_FLEXION VARCHAR(50),
+    Elbow_FLEXION VARCHAR(50),
     Controller_MODE VARCHAR(50),
     Controller_CUSHION VARCHAR(50),
     Controller_SENSITIVITY VARCHAR(50),
-    Controller_ROM_LIMIT VARCHAR(50),
+    Controller_ROM_UPPER_LIMIT VARCHAR(50),
+    Controller_ROM_LOWER_LIMIT VARCHAR(50),
     Controller_ROM_PERCENTAGE VARCHAR(50),
     Controller_ECCENTRIC_SPEED VARCHAR(50),
     Controller_PASSIVE_SPEED VARCHAR(50),
@@ -73,35 +75,34 @@ CREATE TABLE settings(
     Controller_PAUSE VARCHAR(50),
     Controller_ISOKINETIC_SPEED VARCHAR(50));
 
-INSERT INTO settings(Powerhead_ORIENTATION, Controller_SENSITIVITY) 
-VALUES ('90Degree', 'Class A');
 SELECT * FROM settings;
 
 
-CREATE TABLE biodex_report(
-    biodexreport_ID BIGSERIAL NOT NULL PRIMARY KEY, 
-    angular_speed REAL [],
-    force REAL [],
-    torque REAL [],
-    exercise VARCHAR(50),
-    repetition VARCHAR(50),
-    muscle VARCHAR(50),
-    settings_ID BIGINT REFERENCES settings(settings_ID));
 
-INSERT INTO biodex_report(exercise, muscle, settings_ID) 
-VALUES ('BicepCurls', 'Bicep, Tricep, Latissimus', 1);
-SELECT * FROM biodex_report;
+CREATE TABLE exercise_data(
+    exercise_ID BIGSERIAL NOT NULL PRIMARY KEY,
+    torque REAL [],
+    angle REAL [],
+    velocity REAL [],
+    muscle VARCHAR(50),
+    exercise VARCHAR(50),
+    repetition VARCHAR(50));
+
+SELECT * FROM exercise_data;
+
+
+
 
 
 CREATE TABLE elgaReport(
     elga_ID BIGSERIAL NOT NULL PRIMARY KEY);
 
-INSERT INTO elgaReport(elga_ID) VALUES ('100');
 SELECT * FROM elgaReport;
 
 
+
 CREATE TABLE personalDATA(
-    SV_Number BIGSERIAL NOT NULL PRIMARY KEY,
+    SV_Number VARCHAR (10) NOT NULL PRIMARY KEY,
     familyStatus VARCHAR(50),
     Email VARCHAR(50),
     adress VARCHAR(50),
@@ -115,8 +116,6 @@ CREATE TABLE personalDATA(
     gender VARCHAR(50),
     NameTitel VARCHAR(50));
 
-INSERT INTO personalDATA(familyStatus, Email, language, telNumber, insurance)
-VALUES ('Mutter', 'technikum@wien.at', 'Deutsch', '0133', 'WGKK');
 SELECT * FROM personalDATA;
 
 
@@ -144,9 +143,9 @@ CREATE TABLE medicalDATA(
     medAnamnesis VARCHAR(50),
     medAdmissionReason VARCHAR(50));
 
-INSERT INTO medicalDATA(hospitalAdress, diagPhysicalIssue, medRiskAlergies)
-VALUES ('Michelbeuern', 'Beinbruch', 'Penicillin');
 SELECT * FROM medicalDATA;
+
+
 
 
 CREATE TABLE mayCONTAIN(
@@ -155,17 +154,24 @@ CREATE TABLE mayCONTAIN(
     elga_ID BIGINT REFERENCES elgaReport(elga_ID),
     PRIMARY KEY (report_ID, biodexreport_ID, elga_ID));
 
-INSERT INTO mayCONTAIN VALUES (1,1,100);
 SELECT * FROM mayCONTAIN;
 
 
 CREATE TABLE givesInforamtion(
     elga_ID BIGINT REFERENCES elgaReport(elga_ID),
-    SV_Number BIGINT REFERENCES personalDATA(SV_Number),
+    SV_Number VARCHAR (10) REFERENCES personalDATA(SV_Number),
     medicalDATA_ID BIGINT REFERENCES medicalDATA(medicalDATA_ID),
     PRIMARY KEY (elga_ID, SV_Number, medicalDATA_ID));
 
-INSERT INTO givesInforamtion VALUES (100,1,1);
 SELECT * FROM givesInforamtion;
+
+
+CREATE TABLE uses(
+    biodexreport_ID BIGINT REFERENCES biodex_report(biodexreport_ID),
+    exercise_ID BIGINT REFERENCES exercise_data(exercise_ID),
+    settings_ID BIGINT REFERENCES settings(settings_ID),
+    PRIMARY KEY (biodexreport_ID, exercise_ID, settings_ID));
+
+SELECT * FROM uses;
 
 
